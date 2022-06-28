10825df1cff71ace8344f7796723b03a21d3fc77
Diogozura <65250673+Diogozura@users.noreply.github.com>
2022-06-28 16:21:46 -0300

    .

--- next-sitemap.js
+++
@@ -1,9 +0,0 @@
-const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
-/** @type {import('next-sitemap').IConfig} */
-const config = {
-  siteUrl: siteUrl || 'https://example.com',
-  generateRobotsTxt: true, // (optional)
-  // ...other options
-}
-
-export default config
